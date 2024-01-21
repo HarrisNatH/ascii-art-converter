6933d2af5dcbaef5dd68c12352f9100b2d817c2f
Harris <harris.hendra@gmail.com>
2024-01-21 16:03:14 +0000

    Added more text in the hello message

--- ascii-art-converter/src/main/java/com.HNH.asciiart/Main.java
+++ ascii-art-converter/src/main/java/com.HNH.asciiart/Main.java
@@ -2,6 +2,6 @@ package com.HNH.asciiart;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, ASCII Art!");
+        System.out.println("Hello, ASCII Art! Extra text");
     }
 }
